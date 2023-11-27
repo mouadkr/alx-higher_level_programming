21091299d2d554b200ebb416573ed3ce1d50c883
mouadkr <dmxmouad@gmail.com>
2023-11-27 11:46:21 -0800

    kol

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Holberton"
+first_3_letters = str[:3]
+last_2_letters = str[-2:]
+middle_word = str[1:-1]
+print(f"First 3 letters: {first_3_letters}")
+print(f"Last 2 letters: {last_2_letters}")
+print(f"Middle word: {middle_word}")
