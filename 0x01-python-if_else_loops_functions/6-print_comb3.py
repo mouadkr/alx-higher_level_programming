3e66a998b39a447020495d41f58d9f8a1752c6e2
mouadkr <dmxmouad@gmail.com>
2023-11-28 10:39:39 -0800

    intell

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i:2d}{j}", end="," if i != 8 or j != 9 else "\n", flush=True)
+        print('{:2d}{}'.format(i, j), end="," if i != 8 or j != 9 else "\n", flush=True)
