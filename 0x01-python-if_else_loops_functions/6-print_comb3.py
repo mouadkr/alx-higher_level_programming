e0e14252ef319733801e957bc3c0a2949714ce96
mouadkr <dmxmouad@gmail.com>
2023-11-28 09:30:26 -0800

    oror

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i < 8 or (i == 8 and j == 9):
-            print(":02d{}".format(i * 10 + j), end=(""))
+            print("{:02d}".format(i * 10 + j), end=(""))
         else:
             print("{:02d}".format(i * 10 + j))
