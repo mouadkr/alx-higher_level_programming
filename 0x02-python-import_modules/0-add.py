1d8cbccbd6164ae3ed5dfe5f57ff708c92b203f4
mouadkr <dmxmouad@gmail.com>
2023-11-30 05:59:47 -0800

    models

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-a = 1
-b = 2
 
 if __name__ == "__main__":
     from add_0 import add
 
-    print(f'{a} + {b} = {add(a, b)}')
+    a = 1
+    b = 2
+
+    print('{:d} + {:d} = {:d}'.format(a,  b, add(a,b)))
